e485ebf56e6158211a9c6ccb8328f48a65048322
magdafairfax <53491450+magdafairfax@users.noreply.github.com>
2019-08-01 12:34:22 +0100

    Load initial project

---
+++ src/main/java/business/WebScraper.java
@@ -0,0 +1,81 @@
+package business;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.*;
+import entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WebScraper {
+    public static void main(String[] args) {
+
+        String baseUrl = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html" ;
+        WebClient client = new WebClient();
+        client.getOptions().setCssEnabled(false);
+        // this example doesn't need javascript and disabling Javascript makes the page load faster in general
+        client.getOptions().setJavaScriptEnabled(false);
+        Pattern pat = Pattern.compile("\\£(-?\\d+.\\d+)?.*");
+
+        try {
+
+            HtmlPage page = client.getPage(baseUrl);
+            List<HtmlElement> items = (List<HtmlElement>) page.getByXPath("//li[@class='gridItem']/div") ;
+            List<Product> productList = new ArrayList();
+
+            if(items.isEmpty()){
+                System.out.println("No items found !");
+            }else {
+                System.out.println("Items found !");
+                for (int i = 0; i <items.size()-3; i++) {
+
+                        String productUrl = ((HtmlAnchor) items.get(i).getFirstByXPath("//li[" + (i+1) + "]//div[not(@valign='top')][@class='productNameAndPromotions']//h3/a")).getHrefAttribute().replace("../","");
+                        // access product description link
+                        HtmlPage  productPage = client.getPage( "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/" + productUrl);
+
+                        HtmlElement htmlProductDetail = productPage.getDocumentElement();
+                        HtmlElement htmlProductTitle =  htmlProductDetail.getFirstByXPath("//div[@class='productTitleDescriptionContainer']");
+                        // get the nutrition table
+                        HtmlTable  htmlNutritionTable = htmlProductDetail.getFirstByXPath("//div//div//table");
+                        // get price per unit
+                        HtmlElement htmlPrice =  htmlProductDetail.getFirstByXPath("//div[@class='pricing']//p[@class='pricePerUnit']");
+                        HtmlElement htmlDescription =  htmlProductDetail.getFirstByXPath("//div[@class='productText']");
+
+                        Product productItem = new Product();
+
+                        productItem.setTitle(htmlProductTitle.asText());
+
+                        if (htmlNutritionTable != null) {
+
+                            productItem.setKcal_per_100g(new Integer(htmlNutritionTable.getCellAt(2,1).asText().replace("kcal","")));
+
+                        }
+
+                        // Extract only the numerical values from the price informations
+                        Matcher mat = pat.matcher(htmlPrice.asText());
+                        while( mat.find()) {
+                            productItem.setUnit_price(new Double(mat.group(1)));
+                        }
+
+                        if(htmlDescription != null) {
+                            productItem.setDescription(htmlDescription.asText());
+                        }
+
+
+                        productList.add(productItem);
+
+                    }
+
+                    ObjectMapper mapper = new ObjectMapper();
+                    String jsonString = mapper.writeValueAsString(productList) ;
+                    System.out.println(jsonString);
+            }
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
